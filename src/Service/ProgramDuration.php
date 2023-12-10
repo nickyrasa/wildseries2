6688b833b220570f88c148992490449592bc3be9
nicky <nickyrasa@gmail.com>
2023-12-10 23:24:56 +0100

    quete20

--- src/Service/ProgramDuration.php
+++ src/Service/ProgramDuration.php
@@ -14,20 +14,21 @@ class ProgramDuration
         $seasons = $program->getSeason();
 
         foreach ($seasons as $season) {
-            $programDuration = $this->calculateAll($season);
+            $seasonDuration = $this->calculateAll($season);
+            $programDuration += $seasonDuration;
         }
         return $programDuration;
     }
 
     public function calculateAll(Season $season): int
     {
-        $programDuration = 0;
+        $seasonDuration = 0;
 
         $episodes = $season->getEpisodes();
 
         foreach ($episodes as $episode) {
-            $programDuration = $episode->getDuration();
+            $seasonDuration += $episode->getDuration();
         }
-        return $programDuration;
+        return $seasonDuration;
     }
 }
